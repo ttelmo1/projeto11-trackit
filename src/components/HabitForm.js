b8d75922907d69166db4a4816fef5f77ead33ae9
ttelmo1 <telmobsj@gmail.com>
2022-12-16 23:18:19 -0300

    Criando habito

---
+++ src/components/HabitForm.js
@@ -0,0 +1,151 @@
+import Input from "./Input"
+import { WEEKDAYS } from "../constants"
+import styled from "styled-components"
+import { useContext, useState } from "react"
+import { UserContext } from "../Providers/UserProvider"
+import axios from "axios"
+
+export default function HabitForm(props) {
+
+    const {setFormOpen} = props;
+    
+    const [habit, setHabit] = useState({ name: "", days: [] });
+    const { currentUser: { token } } = useContext(UserContext);
+
+    function clickCheckbox(a) {
+        const selected = habit.days.includes(a);
+        if (selected) {
+            setHabit({ ...habit, days: habit.days.filter(b => b !== a) })
+        }
+        else {
+            setHabit({ ...habit, days: [...habit.days, a] })
+
+        }
+    }
+
+   
+
+    async function saveHabit (habit) {
+        if(habit.name === ""){
+            alert("Digite o nome do hábito")
+            return;
+        }
+        if(habit.days.length === 0){
+            alert("Selecione pelo menos um dia da semana")
+            return;
+        }
+        try{
+        const config = {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        };
+        const response = await axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", habit, config);
+        setFormOpen(false); 
+        } catch (error) {
+            alert(error.response.data.message);
+        }  
+    }
+
+
+    async function submitHabit(e) {
+        e.preventDefault();
+        await saveHabit(habit);
+            
+    }
+
+    return (
+        <StyledHabitForm onSubmit={submitHabit}>
+            <Input
+                type="text"
+                placeholder="nome do hábito"
+                value={habit.name}
+                onChange={e => setHabit({ ...habit, name: e.target.value })}
+                required
+            />
+            <StyledWeekdayList>
+                {WEEKDAYS.map((a, i) =>
+                    <StyledWeekdaysBox
+                        selected={habit.days.includes(i)}
+                        onClick={() => clickCheckbox(i)}
+                        key={i}
+                    >
+                        {a}</StyledWeekdaysBox>)}
+            </StyledWeekdayList>
+            <ButtonDiv>
+                <p className="cancel" onClick={() => setFormOpen(false)}>Cancelar</p>
+                <button type="submit" className="save">Salvar</button>
+            </ButtonDiv>
+        </StyledHabitForm>
+    )
+}
+
+const StyledHabitForm = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 18px;
+
+    
+`
+
+
+const StyledWeekdayList = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    margin-top: 8px;
+`
+
+const StyledWeekdaysBox = styled.div`
+    width: 30px;
+    height: 30px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-family: "Lexend Deca", sans-serif;
+
+    cursor: pointer;
+    border: 1px solid #D5D5D5;
+
+    font-size: 20px;
+
+    color: ${props => props.selected ? "#fff" : "#DBDBDB"};
+    background-color: ${props => props.selected ? "#CFCFCF" : "#fff"};
+
+`
+
+const ButtonDiv = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 23px;
+    margin-top: 29px;
+
+    .cancel {
+        font-family: "Lexend Deca", sans-serif;
+        font-size: 16px;
+        color: #52B6FF;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+    }
+
+    .save {
+        cursor: pointer;
+        width: 84px;
+        height: 35px;
+        border-radius: 5px;
+        background-color: #52B6FF;
+        color: #fff;
+
+        font-family: "lexend Deca", sans-serif;
+        font-size: 16px;
+        border: none;
+    }
+`
--- src/components/HabitsContainer.js
+++ src/components/HabitsContainer.js
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import styled from "styled-components"
+import HabitForm from "./HabitForm"
+import HabitListed from "./HabitListed";
 
 export default function HabitsContainer() {
+
+    const [habits, setHabits] = useState([]);
+    
+
+    const [formOpen , setFormOpen] = useState(false);
+
+
+
     return (
-        <Container></Container>
+        <Container>
+            <Header>
+                <h2>Meus hábitos</h2>
+                <button onClick={() => setFormOpen(true)}>+</button>
+            </Header>
+            {formOpen && <HabitForm setFormOpen={setFormOpen}/> }
+            {habits.length <= 0 ?  
+                <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p> : 
+                <HabitListed habits={habits} setHabits={setHabits} />
+            }
+        </Container>
     )
 }
 
@@ -11,15 +32,43 @@ const Container = styled.div`
     width: 100%;
     min-height: calc(100vh - 140px);
     background-color: #e5e5e5;
-
+    padding: 22px 18px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    
     gap: 20px;
-
     & > p {
         color: #666666;
         font-size: 18px;
+
+        font-family: 'Lexend Deca', sans-serif;
+    }
+`
+
+const Header = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-family: 'Lexend Deca', sans-serif;
+
+    h2{
+        font-size: 23px;
+        color: #126BA5;
+    }
+
+    button {
+        width: 40px;
+        height: 35px;
+        border-radius: 5px;
+        background-color: #52B6FF;
+        color: #fff;
+
+        font-size: 27px;
+        font-weight: 400;
+        line-height: 34px;
+        
+        border: none;
     }
 `
